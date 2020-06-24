951b7698e1705156e50ab6a7ce8b3b3ab52be88e
kenneththesheep <kenneththesheep@gmail.com>
2020-06-25 00:38:03 +0800

    Updates

---
+++ precache-manifest.ce053e9ea1345abd17e9a7348df9a8bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd78c85ffdaa96cf5957cfaccc4f1b51",
+    "url": "/weatherseason/index.html"
+  },
+  {
+    "revision": "7bec8a773348e4a51f36",
+    "url": "/weatherseason/static/css/main.6aad961e.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/weatherseason/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherseason/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bec8a773348e4a51f36",
+    "url": "/weatherseason/static/js/main.04538a05.chunk.js"
+  },
+  {
+    "revision": "485ac3fdce1c1f8e5187",
+    "url": "/weatherseason/static/js/runtime-main.f4a8a8e4.js"
+  }
+]);
